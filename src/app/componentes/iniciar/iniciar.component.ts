e58ed1655e177e33e89dcd714fffb67f936d9f96
jgranados4 <jgranados@est.ups.edu.ec>
2024-04-20 09:19:35 -0500

    Actualizar estilos y formularios en los componentes iniciar e registrate

--- src/app/componentes/iniciar/iniciar.component.ts
+++ src/app/componentes/iniciar/iniciar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import {
   FormControl,
@@ -6,17 +6,42 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { UsuariosService } from '../../core/services/usuarios.service';
+import { MensajesComponent } from '../mensajes/mensajes.component';
+import { Login, Usuario } from '../../core/models/usuario';
+import { AuthResponse } from '../../core/models/AuthResponse';
 
 @Component({
   selector: 'app-iniciar',
   standalone: true,
-  imports: [NavbarComponent, ReactiveFormsModule],
+  imports: [NavbarComponent, ReactiveFormsModule, MensajesComponent],
   templateUrl: './iniciar.component.html',
   styleUrl: './iniciar.component.css',
 })
 export class IniciarComponent {
+  messageBoolean: boolean = false;
+  GetToken: string = '';
   formgroups = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
+    constrasena: new FormControl('', [Validators.required]),
   });
+  constructor(private usuarios: UsuariosService) {}
+  login(): void {
+    const login: Login<string> = {
+      email: this.formgroups.value.email,
+      constrasena: this.formgroups.value.constrasena,
+    };
+    console.log(login);
+    this.usuarios.Login(login).subscribe({
+      next: (data: AuthResponse) => {
+        let tk = JSON.stringify(data.token);
+        console.log(tk);
+        this.GetToken = JSON.parse(tk);
+        localStorage.setItem('token', this.GetToken);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
