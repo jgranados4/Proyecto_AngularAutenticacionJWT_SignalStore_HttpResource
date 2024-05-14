6290669d766d8918c2ca6612d780d73e935a9c16
jgranados4 <jgranados@est.ups.edu.ec>
2024-05-14 11:57:08 -0500

    feat: Agregar componente de tabla-grid con funcionalidad básica

---
+++ src/app/shared/components/tabla-grid/tabla-grid.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+
+@Component({
+  selector: 'app-tabla-grid',
+  standalone: true,
+  imports: [AgGridAngular, CommonModule],
+  templateUrl: './tabla-grid.component.html',
+  styleUrl: './tabla-grid.component.css',
+})
+export class TablaGridComponent {
+  rowData = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+}
