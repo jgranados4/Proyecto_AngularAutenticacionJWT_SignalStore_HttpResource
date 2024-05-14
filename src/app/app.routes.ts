0b462c08a72049defe4615b0e8f1daad9f774aff
jgranados4 <jgranados@est.ups.edu.ec>
2024-05-14 12:13:55 -0500

    feat: Actualizar rutas de la aplicación para cargar componentes de forma diferida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './componentes/home/home.component';
-import { TablasComponent } from './componentes/tablas/tablas.component';
 import { RegistrateComponent } from './componentes/registrate/registrate.component';
 import { IniciarComponent } from './componentes/iniciar/iniciar.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'Home', component: HomeComponent, canActivate: [authGuard] },
-  { path: 'Tablas', component: TablasComponent, canActivate: [authGuard] },
+  {
+    path: 'Home',
+    loadComponent: () =>
+      import('./componentes/home/home.component').then((m) => m.HomeComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'Tablas',
+    loadComponent: () =>
+      import('./componentes/tablas/tablas.component').then(
+        (m) => m.TablasComponent
+      ),
+    canActivate: [authGuard],
+  },
   { path: 'Registrar', component: RegistrateComponent },
   { path: 'login', component: IniciarComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
