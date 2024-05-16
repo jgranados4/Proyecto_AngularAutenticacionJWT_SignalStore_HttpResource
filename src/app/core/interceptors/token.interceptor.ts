5e5390dfbb085091ff216a137a257c9611920635
jgranados4 <jgranados@est.ups.edu.ec>
2024-05-16 09:27:16 -0500

    feat: :sparkles:
    Actualizar componente de navegación: agregar funcionalidad de cierre de sesión y eliminar componente innecesario

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -5,7 +5,7 @@ import { UsuariosService } from '../services/usuarios.service';
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   //const token = localStorage.getItem('token');
   const usuarios = inject(UsuariosService);
-  const token = usuarios.getToken().token;
+  const token = usuarios.getToken();
   const CloneRequest = req.clone({
     setHeaders: {
       Authorization: `Bearer ${token}`,
--- src/app/core/services/usuarios.service.ts
+++ src/app/core/services/usuarios.service.ts
@@ -5,6 +5,7 @@ import { Login, Usuario } from '../models/usuario';
 import { CookieService } from 'ngx-cookie-service';
 import { jwtDecode, JwtPayload } from 'jwt-decode';
 import { tokenpayload } from '../models/AuthResponse';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,10 +16,11 @@ export class UsuariosService {
   private readonly headers = new HttpHeaders({
     'Content-Type': 'application/json; charset=utf-8',
   });
-  //*inect
+  //*inject
   private _cookies = inject(CookieService);
   private http = inject(HttpClient);
-  constructor() {}
+  //*Observable
+  public exp: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   ObtenerUsuarios() {
     return this.http.get(`${this.URL}/UsuarioAUs`);
   }
@@ -40,23 +42,28 @@ export class UsuariosService {
   setToken(data: any): void {
     return this._cookies.set('token', data);
   }
-  getToken(): { token: string | null; decoded: tokenpayload | null } {
+  getToken(): string | null {
     let token = this._cookies.get('token');
-    let decoded: JwtPayload | null = null;
-    try {
-      if (token) {
-        //enviarlo tambien en el return
-        decoded = jwtDecode<JwtPayload>(token);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    return { token, decoded: decoded as tokenpayload };
+    return token;
   }
   //*Chequear si el token existe
   isAuthenticatedToken(): boolean {
     return this._cookies.check('token');
   }
   //agregar tiempo cookies  y el tiempo viene en el token
-
+  TokenDecoded(): tokenpayload | null {
+    let decoded: JwtPayload | null = null;
+    try {
+      const token = this.getToken();
+      if (token) {
+        //enviarlo tambien en el return
+        decoded = jwtDecode<JwtPayload>(token);
+        console.log('decoded', decoded);
+        this.exp.next((decoded.exp as number) * 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    return decoded as tokenpayload;
+  }
 }
