dee3eee73e59af992e15e395d87e85fc842cbe93
jgranados4 <jgranados@est.ups.edu.ec>
2024-05-16 09:23:28 -0500

    feat: :sparkles:
    Actualizar componente de navegación: agregar funcionalidad de cierre de sesión y eliminar componente innecesario

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,22 +1,55 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UsuariosService } from '../../core/services/usuarios.service';
+import { tokenpayload } from '../../core/models/AuthResponse';
+import { JsonPipe } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  //*Inject
   usuarios = inject(UsuariosService);
+  //*Observables
+  private tokenExpire: Subscription = new Subscription();
+  //*variables
+  Getusuarios: tokenpayload = {};
+  //*iniciar el componente
+  ngOnInit(): void {
+    this.Getusuarios = this.usuarios.TokenDecoded() as tokenpayload;
+    this.tokenExpire = this.usuarios.exp.subscribe((exp) => {
+      console.log('exp', new Date(exp));
+      const expiration = new Date(exp);
+      if (new Date() >= expiration) {
+        this.logout();
+        //recargar la pagina
+        window.location.reload();
+      }
+    });
+  }
   logout(): void {
     console.log('logout');
     this.usuarios.logout();
+    this.isAuthenticated = false;
   }
-  //obtener el nombre del token
-  Token(): string | void {
-    return this.usuarios.getToken();
+  // Token(): void | any {
+  //   console.log(this.usuarios.getToken().decoded?.name);
+  //   return this.usuarios.getToken().token;
+  // }
+  //chequear si el token existe, si existe mostrar el nombre del usuario y el boton de logout
+  //si no existe mostrar el boton de login
+  checkToken(): boolean {
+    return (this.isAuthenticated = this.usuarios.isAuthenticatedToken()
+      ? true
+      : false);
+  }
+  ngOnDestroy(): void {
+    this.tokenExpire.unsubscribe();
   }
 }
