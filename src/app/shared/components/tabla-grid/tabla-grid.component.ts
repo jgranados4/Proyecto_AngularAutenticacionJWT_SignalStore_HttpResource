8df2ec671cabfa0906dec5d09bbeff7fe28e3325
jgranados4 <jgranados@est.ups.edu.ec>
2024-06-01 19:05:27 -0500

    chore: Actualizar estilos del componente tabla-grid y agregar mensajes de error para campos obligatorios

--- src/app/shared/components/tabla-grid/tabla-grid.component.ts
+++ src/app/shared/components/tabla-grid/tabla-grid.component.ts
@@ -1,27 +1,96 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef } from 'ag-grid-community';
+import { ContenidoServicesService } from '../../../core/services/contenido-services.service';
+import { Usuario } from '../../../core/models/usuario';
+import { delay } from 'rxjs';
+import { MessageService } from '../../../core/services/message.service';
+import { MensajesComponent } from '../../../componentes/mensajes/mensajes.component';
 
 @Component({
   selector: 'app-tabla-grid',
   standalone: true,
-  imports: [AgGridAngular, CommonModule],
+  imports: [AgGridAngular, CommonModule, MensajesComponent],
   templateUrl: './tabla-grid.component.html',
   styleUrl: './tabla-grid.component.css',
 })
-export class TablaGridComponent {
-  rowData = [
-    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-  ];
+export class TablaGridComponent implements OnInit {
+  rowDatassignal: WritableSignal<any[]> = signal([]);
+  messageBoolean: boolean = false;
+  //inyection
+  msj = inject(MessageService);
+  contenidos = inject(ContenidoServicesService);
 
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
-    { field: 'electric' },
+    { field: 'id' },
+    {
+      field: 'nombre',
+      filter: true,
+      floatingFilter: true,
+      editable: true,
+      onCellValueChanged: (events) => {
+        console.log('cambio', events.data.nombre);
+        this.contenidos
+          .PutContenido(events.data.id, events.data)
+          .pipe(delay(1000))
+          .subscribe({
+            next: (data: any) => {
+              const message = 'editado con exito';
+              this.msj.sendMessage(message);
+              this.messageBoolean = true;
+              this.ngOnInit();
+            },
+            error: (error) => {
+              console.log(error);
+            },
+          });
+      },
+    },
+    { field: 'apellido' },
+    { field: 'estado' },
+    {
+      field: 'Acciones',
+      cellRenderer: () => {
+        return `<button class="button is-danger">Eliminar</button>`;
+      },
+      onCellClicked: (event) => {
+        console.log('dsfd', event.data.id);
+        this.Eliminar(event.data.id);
+      },
+    },
   ];
+
+  constructor() {}
+  ngOnInit(): void {
+    this.contenidos.GetContenido(1).subscribe({
+      next: (data: any) => {
+        this.rowDatassignal.update(() => data);
+      },
+    });
+  }
+  Eliminar(id: number): void {
+    this.contenidos
+      .DeleteContenido(id)
+      .pipe(delay(1000))
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);
+          const message = 'eliminado con exito';
+          this.msj.sendMessage(message);
+          this.messageBoolean = true;
+          this.ngOnInit();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
 }
