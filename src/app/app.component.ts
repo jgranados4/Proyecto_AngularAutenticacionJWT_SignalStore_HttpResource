caf07a3a7da3d33699ca222651f25f2c9c9dfd7c
jgranados4 <jgranados@est.ups.edu.ec>
2024-06-18 22:25:11 -0500

    chore: Actualizar estilos del componente navbar y agregar funcionalidad de menú desplegable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
