3a87092745422bddc2dd99629ee3aace54acb2b9
jgranados4 <jgranados@est.ups.edu.ec>
2025-05-20 23:03:23 -0500

    chore: Eliminar componente de menú y sus estilos asociados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { MenuComponent } from './componentes/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,MenuComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
