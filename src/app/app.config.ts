a39d9c6779b5fb5fe192a8b74838ebb6d566a106
jgranados4 <jgranados@est.ups.edu.ec>
2025-05-21 21:38:36 -0500

    feat: Añadir ngx-toastr para notificaciones y configurar en app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { errorInterceptorInterceptor } from './core/interceptors/error-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimations(),
+    provideToastr({ timeOut: 900, preventDuplicates: true }),
+    provideHttpClient(withInterceptors([tokenInterceptor,errorInterceptorInterceptor])),
   ],
 };
