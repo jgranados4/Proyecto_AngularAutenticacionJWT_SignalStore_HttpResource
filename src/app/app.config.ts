ed00d9ce325fbd6a2f248be616bb2f5218ecc263
jgranados4 <jgranados@est.ups.edu.ec>
2025-09-17 22:18:40 -0500

    refactor: reorganizar importaciones y agregar manejadores de errores globales y detección de cambios sin zona

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptors/token.interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptorInterceptor } from './core/interceptors/error-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations(),
     provideToastr({ timeOut: 4000, preventDuplicates: true }),
     provideHttpClient(
       withInterceptors([tokenInterceptor, errorInterceptorInterceptor])
     ),
+    provideZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners(),
   ],
 };
